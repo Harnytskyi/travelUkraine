2399e720c892250b9ce0167ebecbbde6417fedbf
Harnytskyi <andrij.garnytskiy@gmail.com>
2021-06-01 22:40:35 +0300

    feat: add styled checkbox component

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,20 @@
+/** @jsx createElement */
+/** @jsxFrag createFragment */
+import { createElement, createFragment } from '../../framework/element';
+import styles from './Checkbox.css';
+
+export default function Checkbox({ label = '', value = '', onChange = null, checked = false }) {
+  return (
+    <label className={[styles.check, styles.option]}>
+      <input
+        className={styles.check__input}
+        type="checkbox"
+        value={value}
+        onChange={onChange}
+        checked={checked}
+      />
+      <span className={styles.check__box}></span>
+      {label}
+    </label>
+  );
+}
--- src/components/ShowAvailableKinds.js
+++ src/components/ShowAvailableKinds.js
@@ -2,20 +2,18 @@
 /** @jsxFrag createFragment */
 import { changeStatus, checkStatus } from '../data/regionData';
 import { createElement, createFragment } from '../framework/element';
+import Checkbox from './Checkbox';
 
 export function ShowAvailableKinds() {
   return (
     <>
       {Object.keys(window.dataStore.availableKinds).map(item => (
-        <label>
-          <input
-            type="checkbox"
-            value={item}
-            onchange={e => changeStatus(e.target.value)}
-            checked={checkStatus(item)}
-          />
-          {item}
-        </label>
+        <Checkbox
+          label={item}
+          value={item}
+          onChange={e => changeStatus(e.target.value)}
+          checked={checkStatus(item)}
+        />
       ))}
     </>
   );
